c098a707fa1eb2fc10a2f9ca9c9acf439f416649
aldm <d44.a1@yandex.ru>
2024-09-26 19:17:31 +0300

    1

---
+++ js/swiper.js
@@ -0,0 +1,72 @@
+// const swiper = new Swiper(".products-price-swiper", {
+//   direction: "horizontal",
+//   loop: false,
+//   slidesPerView: 4,
+//   spaceBetween: 40,
+
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 40,
+//     },
+//     375: {
+//       slidesPerView: 1,
+//       spaceBetween: 40,
+//     },
+//     425: {
+//       slidesPerView: 1,
+//       spaceBetween: 40,
+//     },
+//     500: {
+//       slidesPerView: 1,
+//       spaceBetween: 40,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 10,
+//     },
+//     769: {
+//       slidesPerView: 2.5,
+//       spaceBetween: 10,
+//     },
+//     835: {
+//       slidesPerView: 3.3,
+//       spaceBetween: 40,
+//     },
+//     1280: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//     1920: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+const swiper = new Swiper(".products-price-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
