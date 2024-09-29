74ccb9a88d633bc382a46d9707cf7b6aa84ae373
aldm <d44.a1@yandex.ru>
2024-09-29 21:58:41 +0300

    1

--- js/swiper.js
+++ js/swiper.js
@@ -1,54 +1,3 @@
-// const swiper = new Swiper(".products-price-swiper", {
-//   direction: "horizontal",
-//   loop: false,
-//   slidesPerView: 4,
-//   spaceBetween: 40,
-
-//   breakpoints: {
-//     320: {
-//       slidesPerView: 1,
-//       spaceBetween: 40,
-//     },
-//     375: {
-//       slidesPerView: 1,
-//       spaceBetween: 40,
-//     },
-//     425: {
-//       slidesPerView: 1,
-//       spaceBetween: 40,
-//     },
-//     500: {
-//       slidesPerView: 1,
-//       spaceBetween: 40,
-//     },
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 10,
-//     },
-//     769: {
-//       slidesPerView: 2.5,
-//       spaceBetween: 10,
-//     },
-//     835: {
-//       slidesPerView: 3.3,
-//       spaceBetween: 40,
-//     },
-//     1280: {
-//       slidesPerView: 4,
-//       spaceBetween: 40,
-//     },
-//     1920: {
-//       slidesPerView: 4,
-//       spaceBetween: 40,
-//     },
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
 const swiper = new Swiper(".products-price-swiper", {
   // Optional parameters
   direction: "horizontal",
@@ -91,6 +40,68 @@ const swiper = new Swiper(".products-price-swiper", {
     1360: {
       slidesPerView: 4,
       spaceBetween: 40,
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+//licenses__swiper
+const swiperlicenses = new Swiper(".licenses-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 6,
+  spaceBetween: 18,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+
+    767: {
+      slidesPerView: 3.2,
+      spaceBetween: 40,
+    },
+    820: {
+      slidesPerView: 3.5,
+      spaceBetween: 20,
+    },
+    910: {
+      slidesPerView: 3.8,
+      spaceBetween: 20,
+    },
+    980: {
+      slidesPerView: 4.2,
+      spaceBetween: 20,
+    },
+    1060: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 5.1,
+      spaceBetween: 20,
+    },
+    1290: {
+      slidesPerView: 5.5,
+      spaceBetween: 20,
+    },
+    1412: {
+      slidesPerView: 6,
+      spaceBetween: 18,
     },
   },
   // Navigation arrows
