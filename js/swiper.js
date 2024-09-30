769df639d8c4aa1e664ea6a964d097d5307e5d7a
aldm <d44.a1@yandex.ru>
2024-09-30 22:49:07 +0300

    1

--- js/swiper.js
+++ js/swiper.js
@@ -44,8 +44,8 @@ const swiper = new Swiper(".products-price-swiper", {
   },
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next btn-mobile-next",
+    prevEl: ".swiper-button-prev btn-mobile-prev",
   },
 
   // And if we need scrollbar
